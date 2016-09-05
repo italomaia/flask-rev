793a331970e7bf42ceb9b5ff88e3ebc53d2116b7
Italo Maia <italo.maia@gmail.com>
2016-09-04 21:54:53 -0300

    adding some docstrings

--- flask_rev.py
+++ flask_rev.py
@@ -1,3 +1,7 @@
+"""
+Main module.
+"""
+
 from flask.helpers import safe_join
 from flask import request
 import hashlib
@@ -5,7 +9,17 @@ import contextlib
 
 
 class Rev(object):
+    """
+    Patches url_for function for the .static endpoint.
+    """
+
     def __init__(self, app=None):
+        """
+        If app is provided, initializes extension.
+
+        :param app: Flask application instance
+        :type app: flask.Flask
+        """
         self._file_hash_cache = {}
         self.app = app
 
@@ -13,7 +27,14 @@ class Rev(object):
             self.init_app(app)
 
     def init_app(self, app):
-        if app.debug: return
+        """
+        Initializes extension with app
+
+        :param app: Flask application instance
+        :type app: flask.Flask
+        """
+        if app.debug:  # don't mess with debug
+            return
 
         @app.url_defaults
         def hashed_url_for_static_file(endpoint, values):
@@ -22,7 +43,7 @@ class Rev(object):
                 if filename:
                     # has higher priority
                     blueprint = endpoint.rsplit('.', 1)[0] \
-                        if '.' in endpoint else request.blueprint  # can be None too
+                        if '.' in endpoint else request.blueprint
 
                     # file from blueprint or project?
                     static_folder = app.blueprints[blueprint].static_folder \
@@ -37,16 +58,26 @@ class Rev(object):
                     values[param_name] = self.get_file_hash(filepath)
 
     def get_file_hash(self, filepath):
+        """
+        Returns file hash from cache.
+
+        :param filepath: path to static file
+        :type filepath: str
+        """
         if filepath not in self._file_hash_cache:
             self._file_hash_cache[filepath] = self.static_file_hash(filepath)
         return self._file_hash_cache[filepath]
 
     @staticmethod
     def static_file_hash(filepath):
-        "Calculates file's md5 hash"
+        """
+        Calculates file hash. Uses md5 algorithm.
+
+        :param filepath: path to static file
+        :type filepath: str
+        """
         hasher = hashlib.md5()
 
         with contextlib.closing(open(filepath, 'rb')) as file:
             hasher.update(file.read())
         return hasher.hexdigest()
-
